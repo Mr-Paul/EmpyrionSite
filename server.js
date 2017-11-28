49e14a749adb7c156830f13f126125bed7779c83
Mr-Paul <heppner.paul@gmail.com>
2017-11-28 15:30:28 -0500

    initial commit, added login/logout, forum start page

---
+++ server.js
@@ -0,0 +1,111 @@
+// Developed by pH Labs for Factions Gaming
+// CTRL + M + L to collapse!
+
+
+// Dependencies
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+
+// Startup
+const app = express();
+var pool = mysql.createPool({
+    host: "127.0.0.1",
+    port: 3306,
+    user: "root",
+    password: "Burton69",
+    database: "empyrion"
+})
+pool.getConnection(function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Established connection to database!");
+    }
+});
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
+app.use(session({
+    secret: 'super top secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+}));
+
+
+// My Stuff
+function getUserInfo(req) {
+    if (typeof (req.session.user) == "undefined" || req.session.user == null) {
+        return false;
+    }
+    return req.session.user;
+}
+
+
+// Routes
+app.get('/', function (req, res) {
+    res.render('pages/index', {
+        user: getUserInfo(req)
+    });
+});
+app.get('/login', function (req, res) {
+    res.render('pages/login', {
+        user: getUserInfo(req),
+        message: ""
+    });
+});
+app.post('/login', function (req, res) {
+    var userEmail = req.body.userEmail;
+    var userPass = req.body.userPass;
+    var message = "";
+    pool.query('select * from users where user_email = ?', [userEmail], function (err, result) {
+        if (err) {
+            console.log(err);
+            message = "Could not connect to database. Please try again.";
+        } else {
+            if (result.length > 0) {
+                if (result[0].user_pass == userPass) {
+                    req.session.user = result[0];
+                    res.redirect('/');
+                    return;
+                } else {
+                    var message = "User name and password do not match...";
+                }
+            } else {
+                message = "User not found, try creating a new account.";
+            }
+        }
+        res.render('pages/login', {
+            user: getUserInfo(req),
+            message: message
+        });
+    });
+});
+app.get('/logout', function (req, res) {
+    req.session.user = null;
+    res.render('pages/login', {
+        user: getUserInfo(req),
+        message: "You have successsfully logged out."
+    });
+});
+app.get('/forum', function (req, res) {
+    pool.query('select * from categories', function (err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('pages/forum', {
+                user: getUserInfo(req),
+                result: result
+            });
+        }
+    });
+});
+
+// Start
+app.listen(8082, "127.0.0.1", function () {
+    console.log("Server starting up. Listening on port 8082.");
+});
